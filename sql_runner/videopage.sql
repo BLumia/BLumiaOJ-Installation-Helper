3ac899cdc0c44e3eb673266c7cdc5490def7e668
Gary Wang <wzc782970009@gmail.com>
2017-12-22 16:35:56 +0800

    default time value

--- sql_runner/videopage.sql
+++ sql_runner/videopage.sql
@@ -1,10 +1,10 @@
 set names utf8; 
 
-CREATE TABLE  `videores` (
+CREATE TABLE IF NOT EXISTS `videores` (
   `vid` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(30) NOT NULL,
   `videodesc` varchar(90) NOT NULL,
-  `time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `time` datetime NOT NULL DEFAULT '1970-01-01 00:00:01',
   `url` varchar(80) NOT NULL,
   `status` int(2) NOT NULL DEFAULT '0',
   `pid` int(11) NOT NULL,
